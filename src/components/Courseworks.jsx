a16e1d1eaf7b4d9957243b913d345be5c60e1809
guntoroyk <guntoro.gyk@gmail.com>
2020-05-11 22:36:28 +0700

    updated coursework

--- src/components/Courseworks.jsx
+++ src/components/Courseworks.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
+import React from 'react';
 
 const Course = () => {
-    return (
-        <div className="md:flex mt-4">
-            <div>
-                <div class="mt-6">
-                    <h2 class="font-bold text-lg">Javascript Development Course</h2>
-                    <p class="text-sm">Hacktiv8 Indonesia x Development Circle from Facebook</p>
-                </div>
-                <div class="mt-6">
-                    <h2 class="font-bold text-lg">Baseline: Data, ML, AI</h2>
-                    <p class="text-sm">Introductory to Big Data, Machine Learning, Artificial Intelligence with Google Cloud Platform</p>
-                </div>
-                <div class="mt-6">
-                    <h2 class="font-bold text-lg">Kubernetes on Google Cloud Platform </h2>
-                    <p class="text-sm">Deploying apps to Google Cloud Platform.</p>
-                </div>
-                <div class="mt-6">
-                    <h2 class="font-bold text-lg">Become Azure Cloud Developer</h2>
-                    <p class="text-sm">Online course by dicoding.com</p>
-                </div>
-                <div class="mt-6">
-                    <h2 class="font-bold text-lg">Microsoft Technology Associate </h2>
-                    <p class="text-sm">Networking fundamental certified by Microsoft</p>
-                </div>
-               
-            </div>
-            {/* <div>
+  return (
+    <div className="md:flex mt-4">
+      <div>
+        <div class="mt-6">
+          <h2 class="font-bold text-lg">Javascript Development Course</h2>
+          <p class="text-sm">
+            Hacktiv8 Indonesia x Development Circle from Facebook.
+          </p>
+        </div>
+        <div class="mt-6">
+          <h2 class="font-bold text-lg">Baseline: Data, ML, AI</h2>
+          <p class="text-sm">
+            Introductory to Big Data, Machine Learning, Artificial Intelligence
+            with Google Cloud Platform.
+          </p>
+        </div>
+        <div class="mt-6">
+          <h2 class="font-bold text-lg">
+            Kubernetes on Google Cloud Platform{' '}
+          </h2>
+          <p class="text-sm">Deploying apps to Google Cloud Platform.</p>
+        </div>
+        <div class="mt-6">
+          <h2 class="font-bold text-lg">Become Azure Cloud Developer</h2>
+          <p class="text-sm">Online course by dicoding.com.</p>
+        </div>
+        <div class="mt-6">
+          <h2 class="font-bold text-lg">Microsoft Technology Associate </h2>
+          <p class="text-sm">Networking fundamental certified by Microsoft.</p>
+        </div>
+      </div>
+      {/* <div>
                 <div class="mt-6">
                     <h2 class="font-bold text-lg">Mastering React</h2>
                     <p class="text-sm">codewithmosh.com</p>
@@ -56,9 +62,8 @@ const Course = () => {
                     <p class="text-sm">Ministry of Finance, Indonesia</p>
                 </div>
             </div> */}
-            
-        </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Course
+export default Course;
